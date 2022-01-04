e53dd5db3dff84074815c8813080a6e00881ea81
nirlepshah <nirlepshah@gmail.com>
2022-01-03 20:18:15 -0500

    Add callback to validate password entered by user

--- passport.js
+++ passport.js
@@ -31,7 +31,11 @@ passport.use(new LocalStrategy(
       console.log('incorrect username');
       return callback(null, false, {message: 'Incorrect username or password.'});
     }
-
+    //Call back to validate password 
+    if (!user.validatePassword(password)) {
+      console.log('incorrect password');
+      return callback(null, false, {message: 'Incorrect password.'});
+    }
     console.log('finished');
 
     return callback(null, user);
