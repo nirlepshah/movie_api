cafc9cdf6ad3813ed3761f249914598ce9d7c797
nirlepshah <nirlepshah@gmail.com>
2022-04-11 17:25:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -38,21 +38,21 @@ app.use(express.static(__dirname + '/public'));
 
 // Import CORS library
 
-const cors = require('cors');
+// const cors = require('cors');
 // app.use(cors());
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:4200', 'http://localhost:4200/welcome', 'http://localhost:4200/movies', 'http://localhost:4200/profile', 'https://mymovieapp08.herokuapp.com/'];
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:4200', 'http://localhost:4200/welcome', 'http://localhost:4200/movies', 'http://localhost:4200/profile', 'https://mymovieapp08.herokuapp.com/'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 // Import passport library 
 const passport = require('passport');
